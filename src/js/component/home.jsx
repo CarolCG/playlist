fc264cd98ed7e41318b536a593de8d0be6dc7e2c
Carolina Castro <caritobel1223@gmail.com>
2023-01-12 15:37:30 +0000

    playlist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [playList, setplayList] = useState([])
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+		.then((response) => {return response.json()
+		})
+		.then((data) => {
+			console.log(data);
+			return setplayList(data) 
+		})
+	}, [])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center ">
+		<ul className="list-group list-group-flush">
+		{playList.map((song) => <button className="btn btn-dark text-start rounded-0" type="button" key={song.id}>{song.id} {song.name} -</button>)}
+		</ul>
+		<div className="d-flex justify-content bg-secondary">
+		<i className="fa fa-backward mx-2" style={{width: "50%", height:"25%"}}></i>
+		<i className="fa fa-play" style={{width: "50%", height:"25%"}}></i>
+		{/* <audio  id="reproductor" controls>	</audio> */}
+        <i className="fa fa-forward mx-2" style={{width: "50%", height:"25%"}}></i>
+		</div>
 		</div>
 	);
 };
